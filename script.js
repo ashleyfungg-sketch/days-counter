c07e46753ddf9410c54b1ad33fa2b03ce74554e5
ashleyfungg-sketch <fungashley28@gmail.com>
2024-08-03 13:09:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const daysCounter = document.getElementById('days-counter');
 
-    const startDate = new Date('2024-04-06');
-    const today = new Date();
-    const timeDifference = today - startDate;
-    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-
-    daysCounter.textContent = daysDifference;
-});
